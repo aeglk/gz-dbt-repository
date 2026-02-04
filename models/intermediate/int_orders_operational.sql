7efaeb412e7c29a6355af471b9d83e9b380b1240
andreaengelke99@gmail.com <andreaengelke99@gmail.com>
2026-02-04 16:18:48 +0000

    finance days

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -5,6 +5,9 @@ SELECT
     revenue,
     quantity,
     purchase_cost,
+    shipping_fee,
+    logcost,
+    ship_cost
 
 FROM {{ ref('int_orders_margin') }}
 LEFT JOIN {{ ref('stg_raw__ship') }} USING (orders_id)
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,11 +1,11 @@
 SELECT
-    date_date AS DATE,
-    COUNT(orders_id) AS Total_Nb_transactions,
-    ROUND(SUM(revenue),0) AS Total_revenue,
+    date_date AS date,
+    COUNT(orders_id) AS total_nb_transactions,
+    ROUND(SUM(revenue),0) AS total_revenue,
     SUM(revenue)/SUM(quantity) AS average_basket,
-    ROUND(SUM(Operational_margin),0) AS total_margin,
+    ROUND(SUM(operational_margin),0) AS total_margin,
     ROUND(SUM(purchase_cost),0) AS Total_purchase_cost,
     ROUND(SUM(shipping_fee),0) AS Total_shipping_fee,
-    ROUND(SUM(logcost),0) AS Total_log_costs,
+    ROUND(SUM(logcost),0) AS total_logcosts,
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date

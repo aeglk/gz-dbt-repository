5afa06d3ecdaa9e677eaf2443cc081a7ba0d249d
andreaengelke99@gmail.com <andreaengelke99@gmail.com>
2026-02-04 16:11:34 +0000

    finance_days

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,11 @@
+SELECT
+    date_date AS DATE,
+    COUNT(orders_id) AS Total_Nb_transactions,
+    ROUND(SUM(revenue),0) AS Total_revenue,
+    SUM(revenue)/SUM(quantity) AS average_basket,
+    ROUND(SUM(Operational_margin),0) AS total_margin,
+    ROUND(SUM(purchase_cost),0) AS Total_purchase_cost,
+    ROUND(SUM(shipping_fee),0) AS Total_shipping_fee,
+    ROUND(SUM(logcost),0) AS Total_log_costs,
+FROM {{ ref('int_orders_operational') }}
+GROUP BY date_date
